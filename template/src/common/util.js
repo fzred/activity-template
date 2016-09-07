2956c9edb4a94d169e688a246c523a0331b67179
fzred <1179750402@qq.com>
2016-09-07 15:13:25 +0800

    update

--- template/src/common/util.js
+++ template/src/common/util.js
@@ -1,8 +1,8 @@
 import cookie from './cookie'
-// ALLPYRA_TERMINAL
-var isApp = !!cookie.get("BD-EGO_TERMINAL")
-function goToLogin() {
-    if (isApp) {
+import wxTool from './wx.tool'
+
+export function goToLogin() {
+    if (isApp()) {
         // 调用APP登录
         let appJson = {
             type: 440,
@@ -13,4 +13,47 @@ function goToLogin() {
     }
 }
 
-module.exports = { goToLogin, isApp }
+export function isApp() {
+    return !!cookie.get("BD-EGO_TERMINAL")  //  ALLPYRA_TERMINAL
+}
+
+/*
+ {
+ link: undefined,
+ title: undefined,
+ desc: undefined,
+ imgUrl: undefined
+ }
+ */
+export function setShare(option) {
+    if (isApp()) {
+        var appJson = {
+            type: 601,
+            obj: {
+                content: option.link,
+                title: option.title,
+                desc: option.desc,
+                image: option.imgUrl
+            }
+        }
+        window.location.href = `/?appJson=${encodeURIComponent(JSON.stringify(appJson))}`
+    } else {
+        wxTool.setShare({
+            imgUrl: option.imgUrl,
+            link: option.link,
+            desc: option.desc,
+            title: option.title
+        })
+    }
+}
+
+export function goHome() {
+    if (isApp()) {
+        location.href = '/?appJson=' +
+            encodeURIComponent(JSON.stringify({
+                type: 100,
+            }))
+    } else {
+        location.href = '/'
+    }
+}
--- template/src/store/index.js
+++ template/src/store/index.js
@@ -1,30 +1,6 @@
-import cookie from "../common/cookie.js";
-import util from '../common/util'
+import { isApp } from '../common/util'
 
-var store = {};
-store.isInApp = util.isInApp
+const store = {}
+store.isInApp = isApp()
 
-store.goHome = function () {
-    if (store.isInApp) {
-        location.href = '/?appJson=' +
-            encodeURIComponent(JSON.stringify({
-                type: 100,
-            }))
-    } else {
-        location.href = '/'
-    }
-}
-
-store.goToLogin = function () {
-    if (util.isInApp) {
-        // 调用APP登录
-        let appJson = {
-            type: 440,
-        }
-        window.location.href = `/?appJson=${encodeURIComponent(JSON.stringify(appJson))}`
-    } else {
-        window.location.href = `/login.html?sourceurl=${encodeURIComponent(window.location.href)}`
-    }
-}
-
-export default store;
+export default store
